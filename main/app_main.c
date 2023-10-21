e74433cd41794b23c0099529aafd8663df5a19fb
CHOTIKA040 <109026153+CHOTIKA040@users.noreply.github.com>
2023-10-21 15:52:15 +0700

    Button Interrupt

--- main/app_main.c
+++ main/app_main.c
@@ -1,12 +1,3 @@
-/* MQTT (over TCP) Example
-
- This example code is in the Public Domain (or CC0 licensed, at your option.)
-
- Unless required by applicable law or agreed to in writing, this
- software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- CONDITIONS OF ANY KIND, either express or implied.
- */
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -31,7 +22,31 @@
 #include "mqtt_client.h"
 
 #include "driver/gpio.h"
+#include "esp_rom_gpio.h"
 static const char *TAG = "MQTT_EXAMPLE";
+
+#define INPUT_PIN 21
+#define LED_PIN 23
+int state = 0;
+QueueHandle_t interputQueue;
+
+// interrupt service handler
+static void IRAM_ATTR gpio_interrupt_handler(void *args) {
+	int pinNumber = (int) args;
+	xQueueSendFromISR(interputQueue, &pinNumber, NULL);
+}
+
+// LED control task, received button prerssed from ISR
+void LED_Control_Task(void *params) {
+	int pinNumber = 0;
+	while (true) {
+		if (xQueueReceive(interputQueue, &pinNumber, portMAX_DELAY)) {
+			gpio_set_level(LED_PIN, gpio_get_level(LED_PIN) == 0);
+			printf("GPIO %d was pressed. The state is %d\n", pinNumber,
+					gpio_get_level(LED_PIN));
+		}
+	}
+}
 
 static void log_error_if_nonzero(const char *message, int error_code) {
 	if (error_code != 0) {
@@ -302,6 +317,24 @@ void app_main(void) {
 	gpio_set_direction(32, GPIO_MODE_OUTPUT);
 	gpio_set_direction(33, GPIO_MODE_OUTPUT);
 	gpio_set_direction(25, GPIO_MODE_OUTPUT);
+	// LED config as I/O port
+	esp_rom_gpio_pad_select_gpio(LED_PIN);
+	gpio_set_direction(LED_PIN, GPIO_MODE_INPUT_OUTPUT);
+
+	// config Input No 21 for external interrupt input
+	esp_rom_gpio_pad_select_gpio(INPUT_PIN);
+	gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
+	gpio_pulldown_en(INPUT_PIN);
+	gpio_pullup_dis(INPUT_PIN);
+	gpio_set_intr_type(INPUT_PIN, GPIO_INTR_POSEDGE);
+
+	// FreeRTOS tas and queue
+	interputQueue = xQueueCreate(10, sizeof(int));
+	xTaskCreate(LED_Control_Task, "LED_Control_Task", 2048, NULL, 1, NULL);
+
+	// install isr service and isr handler
+	gpio_install_isr_service(0);
+	gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void*) INPUT_PIN);
 	ESP_LOGI(TAG, "[APP] Startup..");
 	ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
 	ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
